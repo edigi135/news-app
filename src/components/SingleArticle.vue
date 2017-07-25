d43b6f84c8564eb793b340a76f087d42398a178e
edigiambattista <edigiambattista@cainc.com>
2017-07-25 13:53:31 -0400

    Update articleView hidden display

--- src/components/SingleArticle.vue
+++ src/components/SingleArticle.vue
@@ -5,18 +5,18 @@
         <div class="card-content">
           <div class="media is-marginless">
             <div class="media-left">
-              <figure class="image is-96x96">
+              <figure class="image is-64x64">
                 <a :href="article.urlToImage"><img :src="this.article.urlToImage === null ? source.urlsToLogos.medium : article.urlToImage" alt="Image not available"></a>
               </figure>
             </div>
             <div class="media-content">
               <p class="title is-marginless is-4"><a :href="article.url">{{ article.title }}</a></p>
-              <p class="subtitle is-marginless is-6"><small>by</small> {{ article.author }}</p>
-              <small><strong>{{ date }}  @  {{ time }}</strong></small>
             </div>
           </div>
           <transition name="slide-fade">
             <div v-show="contentActive" class="content">
+              <p class="subtitle is-marginless is-6"><small>by</small> {{ article.author }}</p>
+              <small><strong>{{ date }}  @  {{ time }}</strong></small>
               {{ article.description }}
               <br/>
             </div>
