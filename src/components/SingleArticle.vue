4c812fafa315f0eddaed75cb3f107fbe42ae8a5d
edigiambattista <edigiambattista@cainc.com>
2017-07-25 13:40:16 -0400

    Add transitions

--- src/components/SingleArticle.vue
+++ src/components/SingleArticle.vue
@@ -1,34 +1,41 @@
 <template>
-  <div class="container">
-    <div class="card">
-      <div class="card-content">
-        <div class="media is-marginless">
-          <div class="media-left">
-            <figure class="image is-96x96">
-              <a :href="article.urlToImage"><img :src="this.article.urlToImage === null ? source.urlsToLogos.medium : article.urlToImage" alt="Image not available"></a>
-            </figure>
+  <transition name="fade">
+    <div class="container" @click="contentActive = !contentActive">
+      <div class="card">
+        <div class="card-content">
+          <div class="media is-marginless">
+            <div class="media-left">
+              <figure class="image is-96x96">
+                <a :href="article.urlToImage"><img :src="this.article.urlToImage === null ? source.urlsToLogos.medium : article.urlToImage" alt="Image not available"></a>
+              </figure>
+            </div>
+            <div class="media-content">
+              <p class="title is-marginless is-4"><a :href="article.url">{{ article.title }}</a></p>
+              <p class="subtitle is-marginless is-6"><small>by</small> {{ article.author }}</p>
+              <small><strong>{{ date }}  @  {{ time }}</strong></small>
+            </div>
           </div>
-          <div class="media-content">
-            <p class="title is-marginless is-4"><a :href="article.url">{{ article.title }}</a></p>
-            <p class="subtitle is-marginless is-6"><small>by</small> {{ article.author }}</p>
-            <small><strong>{{ date }}  @  {{ time }}</strong></small>
-          </div>
-        </div>
-
-        <div class="content">
-          {{ article.description }}
-          <br/>
-          <!-- <small><strong>{{ date }}  ||  {{ time }}</strong></small> -->
+          <transition name="slide-fade">
+            <div v-show="contentActive" class="content">
+              {{ article.description }}
+              <br/>
+            </div>
+          </transition>
         </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
 export default {
   name: 'SingleArticle',
   props: ['article', 'source'],
+  data () {
+    return {
+      contentActive: false
+    };
+  },
   computed: {
     dateTimeArr () {
       return this.article.publishedAt.split('T');
@@ -51,4 +58,21 @@ section {
   padding-top: 8px;
   padding-bottom: 2px;
 }
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .25s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0
+}
+.slide-fade-enter-active {
+  transition: all .25s ease;
+}
+.slide-fade-leave-active {
+  transition: all .1s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-active {
+  transform: translateY(-16px);
+  opacity: 0;
+}
+
 </style>
